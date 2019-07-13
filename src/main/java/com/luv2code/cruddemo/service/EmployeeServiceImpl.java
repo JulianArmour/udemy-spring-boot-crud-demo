1e9160dd44e00b36dc6b40d7a02429f0b6afa368
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2019-07-12 20:53:20 -0400

    created the employee repository by extending the JpaRepository Interface

--- src/main/java/com/luv2code/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/cruddemo/service/EmployeeServiceImpl.java
@@ -1,42 +1,41 @@
 package com.luv2code.cruddemo.service;
 
-import com.luv2code.cruddemo.dao.EmployeeDAO;
+import com.luv2code.cruddemo.dao.EmployeeRepository;
 import com.luv2code.cruddemo.entity.Employee;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
-    public EmployeeServiceImpl(@Qualifier("employeeDaoJpaImpl") EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
-    @Transactional
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
-    @Transactional
     public Employee findById(int id) {
-        return employeeDAO.findEmployeeById(id);
+        Optional<Employee> maybeEmployee = employeeRepository.findById(id);
+        if (maybeEmployee.isPresent()) {
+            return maybeEmployee.get();
+        }
+        throw new RuntimeException("No employee with id " + id + " found.");
     }
 
     @Override
-    @Transactional
     public void save(Employee employee) {
-        employeeDAO.saveOrUpdate(employee);
+        employeeRepository.save(employee);
     }
 
     @Override
-    @Transactional
     public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
